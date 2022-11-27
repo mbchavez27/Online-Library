49c5c92958aa901bd17fd1f58033204c9fe61eca
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2022-11-28 07:57:20 +0800

    start book constructor

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ document.querySelector("#addBookButton").addEventListener("click", () => {
   if (addBookForm.style.display == "flex") addBookForm.style.display = "none";
   else addBookForm.style.display = "flex";
 });
+
+//Add Book
+let libraryCollection = [];
+
+function submitBook(title, pages, author, isRead) {
+  this.title = title;
+  this.pages = pages;
+  this.author = author;
+  this.isRead = isRead;
+}
+
+document.querySelector("#submitBook").addEventListener("click", () => {
+  let title = document.querySelector("#addBookTitle").value;
+  let pages = document.querySelector("#addBookAuthor").value;
+  let author = document.querySelector("#addBookPages").value;
+  let isRead = false;
+  if (document.querySelector("#addisBookRead").checked) {
+    isRead = true;
+  } else {
+    isRead = false;
+  }
+
+  submitBook(title, pages, author, isRead);
+});
